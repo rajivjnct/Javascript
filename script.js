5b6806fda5e77ebc7284935d420abc05d26da7d8
Rajiv Kumar06 <rajivk@in.ibm.com>
2021-08-30 18:30:30 +0530

    Javascript01- Javascript Concept

---
+++ script.js
@@ -0,0 +1,11 @@
+//First Code
+//alert("Hello World!!, This is First Code");
+//alert("This is the Another Line of Code");
+
+//Output
+/*document.getElementById("output").innerHTML =
+  "<h1>This Text is coming from JS</h1>";*/
+
+//Prompt
+var name = prompt("What is your name?");
+document.getElementById("output").innerHTML = `My Name is ${name}`;
